0c0b33fd286f138fdf1c0742751ab86864956ff5
diegochagas <drochachagas@gmail.com>
2019-03-17 21:31:29 -0300

    Fix generate telephone and cellphone functions

--- registration-generator.js
+++ registration-generator.js
@@ -5,16 +5,18 @@
 const namesJSON = ["João", "José", "Allison", "Arthur", "Ana", "Alex", "Arlene", "Alberto", "Barry", "Bertha", "Bill", "Bonnie", "Bret", "Beryl", "Chantal", "Cristobal", "Claudette", "Charley", "Cindy", "Chris", "Dean", "Dolly", "Danny", "Danielle", "Dennis", "Debby", "Erin", "Edouard", "Erika", "Earl", "Emily", "Ernesto", "Felix", "Fay", "Fabian", "Frances", "Franklin", "Florence", "Gabielle", "Gustavo", "Grace", "Gaston", "Gert", "Gordon", "Humberto", "Hanna", "Henri", "Hermine", "Harvey", "Helene", "Iris", "Isidore", "Isabel", "Ivan", "Irene", "Isaac", "Jerry", "Josephine", "Juan", "Jeanne", "Jose", "Joyce", "Karen", "Kyle", "Kate", "Karl", "Katrina", "Kirk", "Lorenzo", "Lili", "Larry", "Lisa", "Lee", "Leslie", "Michelle", "Marco", "Mindy", "Maria", "Michael", "Noel", "Nana", "Nicholas", "Nicole", "Nate", "Nadine", "Olga", "Omar", "Odette", "Otto", "Ophelia", "Oscar", "Pablo", "Paloma", "Peter", "Paula", "Philippe", "Patty", "Rebekah", "Rene", "Rose", "Richard", "Rita", "Rafael", "Sebastien", "Sally", "Sam", "Shary", "Stan", "Sandy", "Tanya", "Teddy", "Teresa", "Tomas", "Tammy", "Tony", "Van", "Vicky", "Victor", "Virginie", "Vince", "Valerie", "Wendy", "Wilfred", "Wanda", "Walter", "Wilma", "William", "Kumiko", "Aki", "Miharu", "Chiaki", "Michiyo", "Itoe", "Nanaho", "Reina", "Emi", "Yumi", "Ayumi", "Kaori", "Sayuri", "Rie", "Miyuki", "Hitomi", "Naoko", "Miwa", "Etsuko", "Akane", "Kazuko", "Miyako", "Youko", "Sachiko", "Mieko", "Toshie", "Junko", "Silva", "Ash", "Jalmir", "Rafael", "Bruno", "Daniel", "Mariane", "Andre", "Diego"];
 const familyNamesJSON = ["Cardoso", "Chagas", "Fernandes", "Santana", "Carvalho", "Martins", "Santos", "Boaventura", "Oliveira", "Moraes", "Leão", "Garcia", "Alves", "Costa", "Oliveira", "Martinez", "Novaes", "Oliveira", "Araújo", "Maia", "Vasconcelos", "Gonçalves", "Guimarães", "Menezes", "Sampaio", "Cavalcante", "Lacerda", "Mello", "Moraes", "Muniz", "Figueira", "Paes", "Lima", "Marques", "Duarte", "Vasconcelos", "Vieira", "Souza", "Soares", "Silva", "Duarte", "Morais", "Gomes", "Paiva", "Junqueira", "Queiroz", "Barreto", "Menezzes", "Campos", "Pilar", "Chaves", "Paris", "Lessa", "Galisteu", "Garcia", "Frota", "Araújo", "Xavier", "Ribeiro", "Rodrigues", "Freitas", "Pinheiro", "Moreira", "Dias", "Guimarães", "Nogueira", "Rocha", "Albuquerque", "Neves", "Bernardes", "Pereira", "Camargo", "Alencar", "Batista", "Andrade ", "Reis", "Amaral", "Barcellos", "Monteiro", "Barreto", "Frias", "Medeiros", "Gonçalves", "Ferrari", "Mesquita"];
 const workSegmentJSON = ["Agência De Publicidade", "Alimentos", "Artigos Esportivos", "Autopeças", "Auditoria", "Consultoria", "Bancos De Varejo", "Bebidas Alcoólicas", "Bebidas Não-Alcoólicas", "Calçados", "Cartão De Crédito", "Cia Aérea", "Construção E Incorporação", "Construção Pesada", "Derivado De Petróleo", "Eletrodomésticos", "Eletroeletrônicos", "Equipamento De Telecomunicação", "Fabricante De Material De Construção", "Farmacêutico", "Fast Food", "Fiação E Tecelagem", "Fornecedores De Energia", "Hardware", "Higiene E Limpeza Doméstica", "Higiene Pessoal, Perfumaria E Cosméticos", "Logística", "Metalurgia E Siderurgia", "Montadora/Importadora De Automóveis", "Montadora/Importadora De Caminhões", "Operadora De Telefonia Fixa", "Operadora De Telefonia Móvel", "Papel E Celulose", "Petroquímica", "Previdência Privada", "Provedor De Internet", "Química", "Rede De Hotéis", "Varejo Eletroeletrônicos", "Varejo Material De Construção", "Varejo Supermercados", "Varejo Têxtil", "Seguradoras", "Software", "Vestuário E Confecção", "Contact Center", "Redes Sociais", "Mineração"];
+const businessNameJSON = ['MEI', 'ME', 'EPP', 'EI', 'EIRELI', 'LTDA', 'SA'];
 const brazilianStatesJSON = { acronyms: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
                               names: ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]};
 const CEPsJSON = ["69905038", "69922215", "57325970", "57070528", "69095312", "69020022", "68908199", "68909134", "45310971", "45078618", "60835355", "60511200", "72543204", "72543204", "29306025", "29305602", "75806090", "74982460", "65071480", "65073560", "65916283", "38183187", "39408304", "79333160", "79826020", "78075866", "78715574", "66070660", "66830724", "58059402", "58034575", "50720165", "55150040", "64049484", "64027424", "83505040", "81930374", "24935475", "26290294", "59151500", "59073000", "59078570", "97110370", "91787568", "76824090", "76820268", "69313045", "69314619", "89215440", "88161332", "49045373", "49050563", "77001486", "77016412", "18400803", "18400803", "02841180", "03819130", "09980181", "16901430", "09172422", "11712330"];
+const addressComplementJSON = ['Ap. 107 ', 'Casa 35 ', 'Casa A', 'Casa A', 'Vila', 'Sítio'];
 const internetProvidersJSON = ["gmail.com", "yahoo.com.br", "bol.com.br", "msn.com", "ig.com.br", "globo.com", "oi.com.br", "pop.com.br", "r7.com.br", "folha.com.br", "uol.com", "outlook.com", "zoho.com", "mail.com", "gmx.com", "fastmail.com", "hushmail.com", "inbox.com", "zipmail.com.br", "limao.com.br", "espn.com"];
 const charactersJSON = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\"", "|", ";", ":", "'", "\"", "<", ".", ">", "/", "?"];
 const RGsJSON = ["911225341", "91.122.534-1", "403289440", "4.032.894-40", "418757896", "41.875.789-6", "2977269", "2.977.269", "429434121", "42.943.412-1"];
 const creditCardFlagsJSON = ["visa", "mastercard", "master", "diners", "elo", "amex", "discover", "aura", "jcb", "hipercard"];
 
 /**
- * Most of the functions that generate documents are made in the pattern that you can only
+ * Most of the functions that generate documents are made by default that you can only
  * call the function, and generate a sequence of numbers as strings, or you can generate a
  * document with a mask (dots or slash between the values), inserting the value 'true' in
  * the function call, like 'example(true)'. Demonstrations of the returns of all functions
@@ -134,24 +136,17 @@ function generateBrazilianTituloDeEleitor(hasMask = false) {
   return tituloDeEleitor;
 }
 
-let generateName = () => choseRandomOption(namesJSON);
+const generateName = () => choseRandomOption(namesJSON);
 
-function generateFamilyName(){
-	return choseRandomOption(familyNamesJSON);
-}
+const generateFamilyName = () => choseRandomOption(familyNamesJSON);
 
-function generateWorkSegment(){
-  const segments = workSegmentJSON;
-	const i = generateRandomNumbers(segments.length - 1);
-	return segments[i];
-}
+const generateWorkSegment = () => choseRandomOption(workSegmentJSON);
 
-function generateRazaoSocial (name, familyName){
+function generateRazaoSocial(){
   const segment = generateWorkSegment();
-	const companyName = name + " " + familyName;
-	const type = ['MEI', 'ME', 'EPP', 'EI', 'EIRELI', 'LTDA', 'SA'];
-	var i = generateRandomNumbers(type.length - 1);
-	return companyName + " " + segment + " " + type[i];
+	const companyName = generateName() + " " + generateFamilyName();
+	const type = choseRandomOption(businessNameJSON);
+	return companyName + " " + segment + " " + type;
 }
 
 function calculateDigitInscricaoEstadualSP (...digits) {
@@ -161,7 +156,7 @@ function calculateDigitInscricaoEstadualSP (...digits) {
   return (digit < 10) ? digit : 0;
 }
 
-function generateInscricaoEstadual(inscricaoEstadual = 'Isento', hasMask = false) {
+function generateInscricaoEstadual(hasMask = false, inscricaoEstadual = 'Isento') {
   const states = brazilianStatesJSON.acronyms;
   states.push('SPP');
   const randomIndex = generateRandomNumbers(states.length - 1);
@@ -298,7 +293,7 @@ function generateInscricaoEstadual(inscricaoEstadual = 'Isento', hasMask = false
     case 'PA':
       digits = generateDigits(8);
       digit1 = calculateDigit(9, ...digits);
-      inscricaoEstadual = printDocument(...digits, digit);
+      inscricaoEstadual = printDocument(...digits, digit1);
       if (hasMask) {
         inscricaoEstadual = '' + digits[1] + digits[2] + digits[3] + digits[4] + digits[5] +
           digits[6] + digits[7] + digits[8] + '-' + digit1;
@@ -434,21 +429,7 @@ function generateInscricaoEstadual(inscricaoEstadual = 'Isento', hasMask = false
   return inscricaoEstadual;
 }
 
-function generateBirthDate(){
-    var actualDate = new Date();
-    var maxYear = actualDate.getFullYear() - 18;
-    var minYear = 1900;
-    return generateDate(minYear, maxYear);
-}
-
-function generateExpirationDate(){
-    var actualDate = new Date();
-    var minYear = actualDate.getFullYear() + 1;
-    var maxYear = minYear + generateRandomNumbers(9);
-    return generateDate(minYear, maxYear);
-}
-
-function getTotalOfDaysOfAMonth(month, year){
+function calculateTotalOfDaysOfAMonth(month, year){
   let totalOfDays = 0;
   let isMonthWith31Days = (month === 01) || (month === 03) || (month === 05) || (month === 07) ||
     (month === 08) || (month === 10) || (month === 12);
@@ -471,25 +452,26 @@ function getTotalOfDaysOfAMonth(month, year){
   return totalOfDays;
 }
 
-function generateDate(minYear, maxYear){
+function generateDate(minYear = 1900, maxYear = new Date().getFullYear()){
   let year = Math.floor(Math.random() * (maxYear - minYear + 1)) + minYear;
   let month = generateRandomNumbers(11) + 1;
   let day = 1;
-  day = generateRandomNumbers(getTotalOfDaysOfAMonth(month, year)) + 1;
+  day = generateRandomNumbers(calculateTotalOfDaysOfAMonth(month, year)) + 1;
   if (day < 10) {
     day = "0" + day;
   }
   if (month < 10) {
     month = "0" + month;
   }
-  const fullDate = day + "/" + month + "/" + year;
-  return {day, month, year, fullDate};
+  const date = '' + day + month + year;
+  const dateWithMask = day + "/" + month + "/" + year;
+  return {day, month, year, date, dateWithMask};
 }
 
-function generateCompanyWebsite(name){
-    var websiteName = name.replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
-    var workSegment = generateWorkSegment().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
-    return "www." + websiteName + workSegment + ".com.br"
+function generateCompanyWebsite(isBrazilian = true){
+  var websiteName = generateName().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
+  var workSegment = generateWorkSegment().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
+  return "www." + websiteName + workSegment + ".com" + (isBrazilian ? ".br" : '');
 }
 
 function generateCEP(){
@@ -502,73 +484,70 @@ function generateCEP(){
   return {fullCEP, local, identifier};
 }
 
-function generateAddressComplement(){
-  const complements = ['Ap. 107 ', 'Casa 35 ', 'Casa A', 'Casa A', 'Vila', 'Sítio'];
-  const i = generateRandomNumbers(complements.length - 1);
-  return complements[i];
+const generateAddressComplement = () => choseRandomOption(addressComplementJSON);
+
+function generateEmail(){
+  const emailName = generateName().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
+  const provider = choseRandomOption(internetProvidersJSON);
+  return emailName + "@" + provider;
 }
 
-function generateCompanyEmail(name){
-    const emailName = name.replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
-    const workSegment = generateWorkSegment().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
-    return emailName + "@" + emailName + workSegment + ".com.br"
+function generateCompanyEmail(isBrazilian = true){
+  const emailName = generateName().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
+  const workSegment = generateWorkSegment().replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
+  return emailName + "@" + workSegment + ".com" + (isBrazilian ? ".br" : "");
 }
 
-function generateEmail(name){
-  const emailName = name.replace(/[áéíóúàèìòùâêîôûãõç\s]/g,"").toLowerCase();
-  const providers = internetProvidersJSON;
-  const i = generateRandomNumbers(providers.length - 1);
-  const isBrazilian = (generateRandomNumbers(1) === 1) ? ".br" : "";
-  return emailName + "@" + providers[i] + ".com" + isBrazilian;
-}
-
-function generatePassword() {
+function generatePassword(amountOfDigits = 8) {
   const characters = charactersJSON;
-  const amountOfDigits = 8;
   const amountOfCharacters = characters.length - 1;
   let password = "";
-  for(let i = 0; i <= amountOfDigits; i++) {
+  for(let i = 0; i < amountOfDigits; i++) {
     let index = generateRandomNumbers(amountOfCharacters);
     password += characters[index];
   }
   return password;
 }
 
-function generateRG() {
-    var RGs = RGsJSON;
-    const amountOfRGs = RGs.length - 1;
-    const randomIndex = generateRandomNumbers(amountOfRGs);
-    return RGs[randomIndex];
+function generateRG (hasMask = false) {
+  let rg = choseRandomOption(RGsJSON);
+  return hasMask ? rg : rg.replace(/[.-\s]/g, '');
 }
 
 function generateStates(){
-  const names = brazilianStatesJSON.names;
-  const acronyms = brazilianStatesJSON.acronyms;
-  const amountOfStates = nomes.length - 1;
-  const randomIndex = generateRandomNumbers(amountOfStates);
-  const name = names[randomIndex];
-  const acronym = acronyms[randomIndex];
+  const name = choseRandomOption(brazilianStatesJSON.names);
+  const acronym = choseRandomOption(brazilianStatesJSON.acronyms);
   return {name, acronym};
 }
 
-function generateTelephone(){
+function generateTelephone(hasMask = false){
   const ddd = Math.floor(Math.random() * (99 - 11 + 1)) + 11;
   const digits = generateDigits(7);
-  let telephone = "" + ddd;
-  for(let i = 0; i < digits.length; i++) {
-    telephone += digits[i];
-  }
   digits[0] = 3;
+  let telephone = "+55";
+  if(hasMask) {
+    telephone += `(${ddd})${digits[0]}${digits[1]}${digits[2]}${digits[3]}-${digits[4]}${digits[5]}${digits[6]}${digits[7]}`;
+  } else {
+    telephone += ddd;
+    for(let i = 0; i < digits.length; i++) {
+      telephone += digits[i];
+    }
+  }
   return telephone;
 }
 
-function generateCellphone(){
+function generateCellphone(hasMask = false){
   const ddd = Math.floor(Math.random() * (99 - 11 + 1)) + 11;
   const digits = generateDigits(8);
-  let cellphone = "" + ddd;
   digits[0] = "9";
-  for(let i = 0; i < digits.length; i++) {
-    cellphone += digits[i];
+  let cellphone = "+55";
+  if(hasMask) {
+    cellphone += `(${ddd})${digits[0]}${digits[1]}${digits[2]}${digits[3]}${digits[4]}-${digits[5]}${digits[6]}${digits[7]}${digits[8]}`;
+  } else {
+    cellphone += ddd;
+    for(let i = 0; i < digits.length; i++) {
+      cellphone += digits[i];
+    }
   }
   return cellphone;
 }
